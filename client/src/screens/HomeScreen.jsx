be21bd98f90f728adac1b4c7641889e80a419edf
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-23 23:38:14 +0530

    mange state by useReducer

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,19 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const type = {
+  fetchRequest: 'FETCH_REQUEST',
+  fetchSuccess: 'FETCH_SUCCESS',
+  fetchFailed: 'FETCH_FAILED',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case type.fetchRequest:
+      return { ...state, loading: true };
+    case type.fetchSuccess:
+      return { ...state, products: action.payload, loading: false };
+    case type.fetchFailed:
+      return { ...state, loading: false, error: action.payload };
+    default: {
+      return state;
+    }
+  }
+};
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: '',
+    products: [],
+  });
+  // const [products, setProducts] = useState([]);
   // console.log(products);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get('/api/products');
-      // console.log(res);
-      setProducts(res.data);
+      dispatch({ type: type.fetchRequest });
+      try {
+        const res = await axios.get('/api/products');
+        dispatch({ type: type.fetchSuccess, payload: res.data });
+      } catch (error) {
+        dispatch({ type: type.fetchFailed, payload: error });
+      }
     };
     fetchData();
   }, []);
+  if (loading) {
+    return (
+      <>
+        <h1>Featured Products</h1>
+        <div className="products">
+          <p>Loading.....</p>
+        </div>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <h1>Featured Products</h1>
+        <div className="products">
+          <p>{error}</p>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <h1>Featured Products</h1>
