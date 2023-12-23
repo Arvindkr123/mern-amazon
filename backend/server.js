3c5f9038d937ca1e4ce54c35cf9d8648b23fa296
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-23 23:10:13 +0530

    did fetch request from the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
-import data from '../client/src/constants/data.js';
+import data from './data.js';
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+// dotenv.config();
+
 const app = express();
 
+// // Apply the cors middleware
+// app.use(cors());
+
 app.get('/api/products', (req, res) => {
-  res.send(data.products);
+  res.send(data);
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`serve at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import data from '../constants/data';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  // console.log(products);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('/api/products');
+      // console.log(res);
+      setProducts(res.data);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products &&
-          data.products.length > 0 &&
-          data.products.map((product) => {
+        {products &&
+          products.length > 0 &&
+          products.map((product) => {
             return (
               <div key={product.slug} className="product">
                 <Link to={`/product/${product.slug}`}>
