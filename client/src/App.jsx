b684a9ec428c4e50b89a79abe0ee5348eace2402
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-23 23:53:08 +0530

    applied react bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import { HomeScreen, ProductScreen } from './screens';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const App = () => {
   return (
     <div>
       <header>
-        <Link to="/">amazona</Link>
+        <Navbar bg={'dark'} variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+        <Link to="/"></Link>
       </header>
       <main>
         <Routes>
